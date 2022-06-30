65b3313c1808a5972310e939a2e10f2612ce4a26
Zhe Sun <sunzhe1996@gmail.com>
2022-06-30 17:45:14 +0800

    fully featured mint btn

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // config
 const NETWORK_ID = 4;
-const CONTRACT_ADDR = '0x3f179Bb801dd1DF3263724C0AA7a959ACa8A5c08';
-const TOTAL_SUPPLY = 5000;
+// const CONTRACT_ADDR = '0x3f179Bb801dd1DF3263724C0AA7a959ACa8A5c08';
+// const TOTAL_SUPPLY = 5000;
+// const FREE_SUPPLY = 2000;
+// const MAX_MINT = 2;
+// const PRICE = 0.00777;
+const CONTRACT_ADDR = '0xc3f2CC8C9f7F859e1b0F6368d674c8E53F78d0b5';
+const TOTAL_SUPPLY = 10;
+const FREE_SUPPLY = 5;
 const MAX_MINT = 2;
+const PRICE = 0.00777;
+
+
+// lib
 const web3 = new Web3(window.ethereum);
 const contract = new web3.eth.Contract(abi, CONTRACT_ADDR)
+window.userWalletAddress = null
 
 // DOM
 const walletTopBtn = document.getElementById('connect-wallet-top');
 const walletBtn = document.getElementById('connect');
 const walletText = document.getElementById('wallet-text');
+const connectedWallet = document.getElementById('connected-wallet');
+
 const mintBtn = document.getElementById('mint');
 const mintText = document.getElementById('mint-text');
-const connectedWallet = document.getElementById('connected-wallet');
+const minus = document.getElementById('minus');
+const plus = document.getElementById('plus');
+const mintAmount = document.getElementById('mint-amount');
 const mintedText = document.getElementById('minted');
-window.userWalletAddress = null
 
 const loginWithMataMask = async () => {
     walletBtn.classList.add('disabled')
@@ -23,8 +37,8 @@ const loginWithMataMask = async () => {
     let accounts;
     try {
         accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    } catch (e) {
-        console.error(e.message)
+    } catch (err) {
+        alert(err.message)
         walletBtn.classList.remove('disabled')
         walletTopBtn.classList.remove('disabled')
         walletText.innerText = 'Connect Wallet'
@@ -51,7 +65,6 @@ const accountChange = () => {
     });
 }
 
-
 const connectable = () => {
     if (!window.ethereum) {
         alert('MetaMask is not installed')
@@ -66,48 +79,6 @@ const connectable = () => {
     return true
 }
 
-const mint1 = async () => {
-    try {
-        await mint(1);
-    } catch (err) {
-        console.error(err.message)
-    }
-}
-
-const mint2 = async () => {
-    try {
-        await mint(2);
-    } catch (err) {
-        console.error(err.message)
-    }
-}
-
-const mint = async () => {
-    const network = await getID()
-    if (network !== NETWORK_ID) {
-        try {
-            await switchNetwork()
-        } catch (err) {
-            console.error(err.message)
-        }
-    }
-
-    mintBtn.classList.add('disabled')
-    mintText.innerText = "Minting..."
-    if (window.userWalletAddress !== null) {
-        try {
-            const receipt = await contract.methods.mint(2).send({
-                from: window.userWalletAddress
-            })
-
-            await exceedMaxMint(window.userWalletAddress);
-            await wroteMinted();
-        } catch (err) {
-            console.error(err)
-        }
-    }
-}
-
 const getID = async () => {
     return await web3.eth.net.getId()
 }
@@ -117,7 +88,7 @@ const switchNetwork = async () => {
     try {
         id = await web3.eth.net.getId()
     } catch (err) {
-        console.error(err)
+        alert(err.message)
     }
 
     if (id !== NETWORK_ID) {
@@ -127,7 +98,7 @@ const switchNetwork = async () => {
                 params: [{ chainId: `0x${NETWORK_ID}` }]
             })
         } catch (e) {
-            console.error(e.message)
+            alert(e.message)
         }
     }
 
@@ -140,9 +111,12 @@ const minted = async () => {
     return await contract.methods.minted().call();
 }
 
-const wroteMinted = async () => {
-    const mintedNFT = await minted();
+const updateMinted = async () => {
+    const mintedNFT = Number(await minted());
     mintedText.innerText = `${mintedNFT} `;
+    if (mintedNFT == TOTAL_SUPPLY) {
+
+    }
 }
 
 const freeMinted = async () => {
@@ -150,28 +124,102 @@ const freeMinted = async () => {
 }
 
 const addressClaimed = async (addr) => {
-    return await contract.methods.addressClaimed(addr).call();
+    let claimed = 0;
+
+    try {
+        claimed = await contract.methods.addressClaimed(addr).call();
+    } catch (err) {
+        alert(err.message)
+        console.log(err.message)
+    }
+    return claimed
 };
 
 const exceedMaxMint = async (addr) => {
     const claimed = await addressClaimed(addr);
-    console.log(claimed);
-    if (claimed == 2) {
-        mintBtn.classList.add('disabled')
-        mintText.innerText = "Max Mint: 2"
+    if (claimed == MAX_MINT) {
+        return true
     } else {
-        mintBtn.classList.remove('disabled')
-        mintText.innerText = "Mint: 2"
+        return false
     }
 }
 
+const freeMintFinished = async () => {
+    const num = await freeMinted();
+    if (num == FREE_SUPPLY) {
+        return true
+    }
+    return false
+}
+
+const mintFinished = async () => {
+    const num = await minted();
+    if (num == TOTAL_SUPPLY) {
+        return true
+    }
+    return false
+}
+
+const mint = async () => {
+    await switchNetwork();
+
+    const amount = Number(mintAmount.innerText);
+    const claimed = Number(await addressClaimed(window.userWalletAddress));
+
+    if (claimed + amount > MAX_MINT) {
+        alert("Exceed max mint")
+        return
+    }
+
+    const mintedNFT = Number(await minted());
+    if (mintedNFT + amount > TOTAL_SUPPLY) {
+        alert("Exceed total supply")
+        return
+    }
+
+    const freeOver = await freeMintFinished();
+    const freeQuota = FREE_SUPPLY - Number(await freeMinted());
+    let freeAmount = 0;
+    if (window.userWalletAddress !== null) {
+        if (freeQuota > MAX_MINT) {
+            freeAmount = amount;
+        } else if (freeQuota > 0) {
+            freeAmount = freeQuota;
+        } else {
+            freeAmount = 0;
+        }
+
+
+        const requiredValue = (amount - freeAmount) * PRICE;
+        try {
+            await contract.methods.mint(amount).send({
+                from: window.userWalletAddress,
+                value: web3.utils.toWei(requiredValue.toString(), 'ether'),
+            })
+        } catch (err) {
+            alert(err.message)
+        }
+
+        updateMinted();
+    }
+}
+
+const mintAmountBtn = () => {
+    plus.addEventListener('click', () => {
+        mintAmount.innerText = 2
+    })
+    minus.addEventListener('click', () => {
+        mintAmount.innerText = 1
+    })
+}
+
 window.addEventListener('DOMContentLoaded', async (event) => {
+    updateMinted();
     if (connectable()) {
         await switchNetwork()
         await loginWithMataMask()
-        await exceedMaxMint(window.userWalletAddress);
         accountChange()
+        mintAmountBtn()
         mintBtn.addEventListener('click', mint)
-        wroteMinted();
     }
-});
+})
